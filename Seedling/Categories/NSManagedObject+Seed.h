a15b2aac9d788281090c0c9e10d3c38161087fee
Tracey Eubanks <tracey.eubanks@moneydesktop.com>
2012-10-18 13:47:12 -0600

    created category shell for NSManagedObject

---
+++ Seedling/Categories/NSManagedObject+Seed.h
@@ -0,0 +1,4 @@
+@interface NSManagedObject (Seed)
++(id)managedObjectWithDictionary:(NSDictionary *)managedObjectDictionary;
+@end
+
---
+++ Seedling/Categories/NSManagedObject+Seed.m
@@ -0,0 +1,10 @@
+#import "NSManagedObject+Seed.h"
+#import "orchestrate.h"
+
+@implementation NSManagedObject (Seed)
+
++(id)managedObjectWithDictionary:(NSDictionary *)managedObjectDictionary {
+    
+}
+
+@end
